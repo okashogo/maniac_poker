b23831999d709b38305970a617aea503cd46f8d6
oka_shogo <cocorusakana@gmail.com>
2020-08-20 20:03:00 +0900

    画像を追加

--- src/index.tsx
+++ src/index.tsx
@@ -23,10 +23,91 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-const H = "❤︎";
-const D = "♠️";
-const C = "♣️";
-const S = "♦︎";
+// DB構造
+const elements = [
+  './img/Mario.png',
+  './img/DonkeyKong.png',
+  './img/Link.png',
+  './img/Samus.png',
+  './img/darksum.png',
+  './img/Yoshi.png',
+  './img/Kirby.png',
+  './img/Fox.png',
+  './img/Pikachu.png',
+  './img/Luigi.png',
+  './img/Nes.png',
+  './img/Falcon.png',
+  './img/Purin.png',
+  './img/Peach.png',
+  './img/Daisy.png',
+  './img/Copper.png',
+  './img/Ice.png',
+  './img/Seak.png',
+  './img/Zelda.jpg',
+  './img/DocktorMario.png',
+  './img/pichu.png',
+  './img/Falco.png',
+  './img/Marus.png',
+  './img/Marus.png',
+  './img/lucina.png',
+  './img/YaungLink.png',
+  './img/ganon.png',
+  './img/myutwo.png',
+  './img/roi.png',
+  './img/kuromu.png',
+  './img/mrgw.png',
+  './img/metanaito.png',
+  './img/pit.png',
+  './img/burapi.png',
+  './img/zerosam.png',
+  './img/wrio.png',
+  './img/sneak.png',
+  './img/ike.png',
+  './img/poketore.png',
+  './img/dd.jpeg',
+  './img/ryocas.png',
+  './img/sonic.png',
+  './img/dedede.png',
+  './img/pikumin.png',
+  './img/rukario.png',
+  './img/robot.png',
+  './img/tounlink.png',
+  './img/uruhu.png',
+  './img/murabito.png',
+  './img/rockman.png',
+  './img/wiifit.png',
+  './img/rozeta.png',
+  './img/littlemac.png',
+  './img/gekko.png',
+  './img/mii.png',
+  './img/pal.png',
+  './img/packman.jpeg',
+  './img/luhure.png',
+  './img/shuruku.png',
+  './img/jr.png',
+  './img/dackhanto.png',
+  './img/ryu.png',
+  './img/ken.png',
+  './img/cloud.png',
+  './img/kamui.png',
+  './img/beyo.png',
+  './img/inku.png',
+  './img/ridori.png',
+  './img/simon.png',
+  './img/lihitar.png',
+  './img/kingcluru.png',
+  './img/sizue.png',
+  './img/gaogaen.png',
+  './img/packnfl.png',
+  './img/joker.png',
+  './img/yusha.png',
+  './img/banjo.png',
+  './img/tely.png',
+  './img/bereto.png',
+  './img/myenmyuen.jpg',
+];
+// DB構造終了
+
 
 // function draw(cards: any[], deck: any[], select:any[]){
 function draw(cards: any[], select:any[]){
@@ -40,6 +121,12 @@ function draw(cards: any[], select:any[]){
   return cardsTmp;
 }
 
+function Img(props:any){
+  // 最初のnullはカウントしない
+  console.log(props);
+  return <img style={{ width:"80px" }} src={props.src} />;
+}
+
 function bgcChange(key: number,select:any[]){
   if(select.includes(key)){
     return "red";
@@ -48,17 +135,20 @@ function bgcChange(key: number,select:any[]){
 }
 
 function App() {
-  // 初期設定 (DB)
+  // 初期設定
   var cardsFirst:any[] = [];
-  for (let i = 1; i <= 13; i++) {
-      cardsFirst[(i-1)*4 + 0] = {number: i, mark: H};
-      cardsFirst[(i-1)*4 + 1] = {number: i, mark: D};
-      cardsFirst[(i-1)*4 + 2] = {number: i, mark: C};
-      cardsFirst[(i-1)*4 + 3] = {number: i, mark: S};
+  // for (let i = 1; i <= 13; i++) {
+  //     cardsFirst[(i-1)*4 + 0] = {element: mario};
+  //     cardsFirst[(i-1)*4 + 1] = {element: donki};
+  //     cardsFirst[(i-1)*4 + 2] = {element: link};
+  //     cardsFirst[(i-1)*4 + 3] = {element: samus};
+  // }
+  for (let i = 0; i < elements.length; i++) {
+      cardsFirst[i] = {element: elements[i]};
   }
 
   cardsFirst = shuffle(cardsFirst);
-  // DB構造終了
+  // 初期設定終了
 
   var handFirst = cardsFirst.slice(0, 5);
   var deckFirst = cardsFirst.slice(5);
@@ -66,43 +156,6 @@ function App() {
   const [select, setSelect] = useState([null]);
   const [isShow, setShow] = useState(true);
 
-  // const element = (
-  //   <div>
-  //     <div style={{display: "flex"}}>
-  //       {hand.map((data, key) => {
-  //           return (
-  //               <h1 style={{ background: bgcChange(key,select) }}
-  //                   onClick={() => {
-  //                   let newSetSelect:any[] = select.concat();
-  //                   if(!newSetSelect.includes(key)){
-  //                     newSetSelect.push(key);
-  //                   }else{
-  //                     newSetSelect.forEach((item, index) => {
-  //                         if(item === key) {
-  //                             newSetSelect.splice(index, 1);
-  //                         }
-  //                     });
-  //                     console.log(newSetSelect);
-  //                   }
-  //                   setSelect(newSetSelect);
-  //                 }}
-  //               >
-  //                 [{data.mark}{data.number}]
-  //               </h1>
-  //           )
-  //       })}
-  //     </div>
-  //     { isShow &&
-  //       <button onClick={() => {
-  //         setCard(draw(hand, deck, select));
-  //         setSelect([null]);
-  //         setShow(false);
-  //       }}>
-  //       ドロー
-  //       </button>
-  //     }
-  //   </div>
-  // );
   const element = (
     <div>
       <div style={{display: "flex"}}>
@@ -125,7 +178,7 @@ function App() {
                     setSelect(newSetSelect);
                   }}
                 >
-                  [{data.mark}{data.number}]
+                  <Img src={data.element}/>
                 </h1>
             )
           }
